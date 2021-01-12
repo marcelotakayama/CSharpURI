2f71e6215355bd2902bfa788f1912fa7db210a54
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-12 10:59:26 -0300

    Subindo primeiro ex

---
+++ Uri1001/Uri1001/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uri1001 {
+    class Program {
+        static void Main(string[] args) {
+
+            int A = int.Parse(Console.ReadLine());
+
+            int B = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("X = " + (A + B));
+        }
+    }
+}
