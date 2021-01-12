ce2c3c3cb873c503fe6a10dc47eca5a985f18c6a
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-12 11:01:45 -0300

    1001

--- Uri1002/Uri1002/Program.cs
+++ Uri1002/Uri1002/Program.cs
@@ -4,11 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Uri1001 {
+namespace Uri1002 {
     class Program {
         static void Main(string[] args) {
-
-            int A, B; A = Convert.ToInt32(Console.ReadLine()); B = Convert.ToInt32(Console.ReadLine()); int X = A + B; Console.WriteLine("X = " + X);
         }
     }
 }
