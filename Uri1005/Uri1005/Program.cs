0a7e0387ca83356ef4fa4d5ac022d9ec108b9dd4
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-12 11:59:55 -0300

    1005

---
+++ Uri1005/Uri1005/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uri1005 {
+    class Program {
+        static void Main(string[] args) {
+            double a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double media = ((a + b) / 2);
+
+            Console.WriteLine("MEDIA = " + media);
+        }
+    }
+}
