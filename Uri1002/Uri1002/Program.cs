a69e23b260a8bf2c12cac644d5031a54f5f38a6e
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-12 11:20:26 -0300

    1002

--- Uri1002/Uri1002/Program.cs
+++ Uri1002/Uri1002/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +8,11 @@ using System.Threading.Tasks;
 namespace Uri1002 {
     class Program {
         static void Main(string[] args) {
+            double pi = 3.14159;
+            double raio = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double area = pi * Math.Pow(raio, 2);
+
+            Console.WriteLine("A=" + area.ToString("F4"));
         }
     }
 }
