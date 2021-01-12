29696cf2aefa5de688a162ff16f1e38147d83d21
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-12 11:23:45 -0300

    1003

---
+++ Uri1003/Uri1003/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uri1003 {
+    class Program {
+        static void Main(string[] args) {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("SOMA = "+ (a+b));
+        }
+    }
+}
