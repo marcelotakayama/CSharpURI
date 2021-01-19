b3901074f669486bfdd4a7ad91eca864bf281146
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-19 11:31:24 -0300

    organzando as pastas

---
+++ ExerciciosUri/ExerciciosUri/Uri1001.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosUri {
+    class Uri1001 {
+        int A, B; A = Convert.ToInt32(Console.ReadLine()); B = Convert.ToInt32(Console.ReadLine()); int X = A + B; Console.WriteLine("X = " + X);
+    }
+}
